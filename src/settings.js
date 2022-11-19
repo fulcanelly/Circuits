73edd7e48c11182e5b92007fe2e0b8746bb0dc7b
fulcanelly <dallasxfulcanelly@gmail.com>
2022-11-19 02:47:55 +0200

    fut: update settings

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,12 @@
 export const settings = {
     cellSize: 50, 
-    gridSize: 6
+    gridSize: 6,
+
+    colors: {
+        background: '#d3d074',
+        poweredWire: '#e01111', 
+        idleWire: '#464345',
+    }
 }
   
   
